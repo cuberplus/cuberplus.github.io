53645f6601f00cfa6257244cee8e1983b354c62e
mpfeifer1 <mikepf97@gmail.com>
2024-10-21 20:01:54 -0700

    Initial Commit

---
+++ src/Components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FileInput } from './FileInput';
+import "../CSS/Style.css";
+
+function App() {
+  return (
+    <div className="App">
+      <FileInput />
+    </div>
+  );
+}
+
+export default App;
