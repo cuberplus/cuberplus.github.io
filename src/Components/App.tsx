2386ca3927dd44c1c05b2fa6b230a81e4eccc5d4
mpfeifer1 <mikepf97@gmail.com>
2024-10-21 21:08:30 -0700

    Made basic website

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
-import { FileInput } from './FileInput';
 import "../CSS/Style.css";
+import { Row, Col, Card, Navbar, Button } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      <FileInput />
+      <header className={"header"}>
+        <Navbar>
+          <Navbar.Brand>
+            CuberPlus
+          </Navbar.Brand>
+        </Navbar>
+      </header>
+      <Row style={{ margin: 10 }}>
+        <Col className='col-6'>
+          <Card className='p-3'>
+            <a href='/cubeastanalyzer' className='stretched-link'>
+              <h3>
+                Cubeast Analyzer
+              </h3>
+            </a>
+            <img
+              src={require("../Assets/CubeastAnalyzer.png")}
+              style={{ border: '1px solid #000000' }}
+            >
+            </img>
+          </Card>
+        </Col>
+
+        <Col className='col-6'>
+          <Card className='p-3'>
+            <a href='/' className='stretched-link'>
+              <h3>
+                More Coming Soon...
+              </h3>
+            </a>
+          </Card>
+        </Col>
+      </Row>
+
     </div>
   );
 }
